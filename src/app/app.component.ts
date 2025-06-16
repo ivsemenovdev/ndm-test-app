6918bbdde8bd4e3ce823026509e791a8069298c2
Ivan Semenov <ivsemenovdev@gmail.com>
2025-06-16 14:47:36 +0300

    add table component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TableComponent } from "./components/table/table.component";
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [TableComponent, NzLayoutModule]
 })
 export class AppComponent {
 }
