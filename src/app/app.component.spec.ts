f34da2c477dacb0d613df043629ac2b868b86daa
Ivan Semenov <ivsemenovdev@gmail.com>
2025-06-16 17:41:02 +0300

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { TableComponent } from './components/table/table.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, TableComponent, NzLayoutModule],
     }).compileComponents();
   });
 
@@ -14,16 +16,38 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ndm-test-app' title`, () => {
+  it('should have correct number of columns', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ndm-test-app');
+    expect(app.listOfColumns.length).toBe(3);
   });
 
-  it('should render title', () => {
+  it('should have correct column titles', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ndm-test-app');
+    const app = fixture.componentInstance;
+    expect(app.listOfColumns[0].title).toBe('Адрес назначения');
+    expect(app.listOfColumns[1].title).toBe('Шлюз');
+    expect(app.listOfColumns[2].title).toBe('Интерфейс');
+  });
+
+  it('should have routes data', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.listOfRoutes.length).toBeGreaterThan(0);
+    const firstRoute = app.listOfRoutes[0];
+    expect('uuid' in firstRoute).toBeTruthy();
+    expect('address' in firstRoute).toBeTruthy();
+    expect('gateway' in firstRoute).toBeTruthy();
+    expect('interface' in firstRoute).toBeTruthy();
+  });
+
+  it('should have correct first route data', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const firstRoute = app.listOfRoutes[0];
+    expect(firstRoute.uuid).toBe('1');
+    expect(firstRoute.address).toBe('0.0.0.0/16');
+    expect(firstRoute.gateway).toBe('193.0.174.1');
+    expect(firstRoute.interface).toBe('Подключение Ethernet');
   });
 });
--- src/app/shared/models/route.model.ts
+++ src/app/shared/models/route.model.ts
@@ -1,3 +1,4 @@
+// TODO: предлагаю изменить название интерфейса на NetworkRoute, так как возможно будут возникать ошибки при импорте (Route есть в angular/router)
 export interface Route {
     uuid: string;
     address: string;
